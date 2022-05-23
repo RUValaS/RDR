e006bba50130503f06f733430c6d52c30e57e454
RUValaS <RUValaS@users.noreply.github.com>
2022-05-23 17:13:02 +0200

    nik les asv (+psnr)

--- Kalman/dispKalman_t.m
+++ Kalman/dispKalman_t.m
@@ -4,7 +4,7 @@ function  dispKalman_t(X,tX,N,Mx,My)
 figure();
 for k=1:N
     subplot(2,N,k);imshow(y_to_im(abs(tX(:,k)),Mx,My));colorbar;axis image;colormap("parula")
-    subplot(2,N,k+N);imshow(y_to_im(abs(X(:,k)),Mx,My));colorbar;axis image;colormap("parula")
+    subplot(2,N,k+N);imshow(y_to_im(abs(X(:,k)),Mx,My));colorbar;axis image;colormap("parula");title(psnr(X(:,k),tX(:,k)))
 end
 end
 
