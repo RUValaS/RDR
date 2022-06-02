b32e0f8cb2f3b17cf1b87df444c67a2eb94b385b
RUValaS <RUValaS@users.noreply.github.com>
2022-06-02 15:35:03 +0200

    ltx + err H

--- main_2.m
+++ main_2.m
@@ -10,7 +10,7 @@ c= 3e8;
 f = 1e9;
 lambda = c/f;
 dist = 100*lambda/2.1; % J_b antennes espacées de dist (normalisé)
-N = 9;
+N = 15;
 SNR = 10;
 iMEM = 1; % nombre itérations MEM 
 RATIO = 10e14; % ratio erreur Kalman
@@ -38,4 +38,4 @@ A_ev(7,1) = 1; A_ev(4,2)=1; A_ev(1,3)=1; A_ev(8,4)=1;A_ev(5,5)=1;A_ev(2,6)=1;A_e
 
 %%% Kalman direct en fait mdr
 [X,tX] = KalmainV4(vadapted,J,z,I,N,f,c,lambda,Mx,My,iMEM,SNR,A_ev,RATIO,mode);
-dispKalman_t(X,tX,N,Mx,My);
+% dispKalman_t(X,tX,N,Mx,My);
